3f4b283111bf08635a98183310e6818bbd38f310
Monkeybtm6 <Monkeybtm6@gmail.com>
2016-09-18 01:12:41 -0700

    Wad maker script, Requires 7zip

---
+++ Makewad.bat
@@ -0,0 +1,19 @@
+ @echo off
+ set ProjName=MonRpg
+ echo Ready to Start.
+ 
+ echo Step 1: Packing
+ echo Packing...
+ cd %cd%
+ 7z a -r -ssw -tzip %ProjName%n.pk3 -xr!.git "*" 
+ echo Done packing.
+ 
+ echo Step 2: Replace old version
+ echo Replacing old version.
+ cd %cd%
+ del %ProjName%.pk3
+ ren %ProjName%n.pk3 %ProjName%.pk3
+ echo Done replacing.
+ 
+:Packingdone
+ echo Done.
